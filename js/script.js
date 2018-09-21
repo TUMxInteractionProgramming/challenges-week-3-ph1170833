d1ff8fcb33133bf22eb09f0c2812c0da5f4dec3f
Nilesh Goel <nilesh.goel11@gmail.com>
2018-09-21 23:42:48 +0530

    Challenge 5+6 completed

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log('app is alive');
+
+function switchChannel(channelName)
+{
+	console.log('Tuning into channel'+channelName);
+ 	document.getElementById('channelhead').innerHTML = channelName;  
+}
